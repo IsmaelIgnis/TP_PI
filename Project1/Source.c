263f9f3a4023184d0163b3616861f4feb4be7b4c
IsmaelIgnis <119451919+IsmaelIgnis@users.noreply.github.com>
2022-12-23 11:04:10 +0000

    Teste

--- Project1/Source.c
+++ Project1/Source.c
@@ -4,14 +4,14 @@ void menu() {
 
     int opcao;
 
-    printf("Bem-vindo. Que operacao deseja realizar?\n");
+    printf("Bem-vindo.\t Que operacao deseja realizar?\n");
     printf(" \n");
     printf("1 - Registar um novo usuario.\n");
     printf("2 - Registar uma nova atividade.\n");
     printf("3 - Ver as atividades planeadas.\n");
     printf("4 - Seguidores.\n");
     printf("5 - Outros.\n");
-    printf("6 - Histórico de informação introduzida.\n");
+    printf("6 - HistÃ³rico de informaÃ§Ã£o introduzida.\n");
 
 
 
@@ -26,26 +26,26 @@ void main() {
 
 }
 
-/*anotações tiradas enquanto fui fazendo o menu:
+/*anotaÃ§Ãµes tiradas enquanto fui fazendo o menu:
 
-Opção 1 do menu: Ter uma função para fazer as informações que vão ser necessárias para serem impressas
+OpÃ§Ã£o 1 do menu: Ter uma funÃ§Ã£o para fazer as informaÃ§Ãµes que vÃ£o ser necessÃ¡rias para serem impressas
 no ficheiro 1 que o prof pede.
 
-Opção 2 - Ao registar a atividade, rgegistá-la num tipo de histórico (necessário uma função para isso)
-para depois só imprimir no segundo ficheiro que o professor pede. Também fazer uma função para dar origem
-às informações do ficheiro 3.
+OpÃ§Ã£o 2 - Ao registar a atividade, rgegistÃ¡-la num tipo de histÃ³rico (necessÃ¡rio uma funÃ§Ã£o para isso)
+para depois sÃ³ imprimir no segundo ficheiro que o professor pede. TambÃ©m fazer uma funÃ§Ã£o para dar origem
+Ã s informaÃ§Ãµes do ficheiro 3.
 
 
-Opção 3 - Ter a função com o calendário, para os pontos 4, 5, 6 e tudo o qiue inclua o utilizador ter de
-colocar um certo período de tempo.
+OpÃ§Ã£o 3 - Ter a funÃ§Ã£o com o calendÃ¡rio, para os pontos 4, 5, 6 e tudo o qiue inclua o utilizador ter de
+colocar um certo perÃ­odo de tempo.
 
-Opção 4 - Funcionalidade de seguir e ver seguidores. Função que mostre quem podemos seguir e quem já
+OpÃ§Ã£o 4 - Funcionalidade de seguir e ver seguidores. FunÃ§Ã£o que mostre quem podemos seguir e quem jÃ¡
 seguimos.
 
-Opção 5 - Tudo o que é coisas para gerar mas que não tenham cenas diretamente relacionadas com as outras
-opções. Ter opção do ponto 7 de ver as médias. Ter opção tambem do ponto 8,10 e 11.
+OpÃ§Ã£o 5 - Tudo o que Ã© coisas para gerar mas que nÃ£o tenham cenas diretamente relacionadas com as outras
+opÃ§Ãµes. Ter opÃ§Ã£o do ponto 7 de ver as mÃ©dias. Ter opÃ§Ã£o tambem do ponto 8,10 e 11.
 
-Opção 6 - Função que apanhe toda a informação inserida pelo utilizador, talvez separadas por categorias.
+OpÃ§Ã£o 6 - FunÃ§Ã£o que apanhe toda a informaÃ§Ã£o inserida pelo utilizador, talvez separadas por categorias.
 Ou seja, "Participantes" e "Atividades"
 
 */
